c3af8e3fd3818d4cf28909c894237f24eb87d4e6
Bruce Schubert <bruce@emxsys.com>
2016-06-03 07:17:18 -0700

    Updated footer text.

--- root/js/viewmodels/footer.js
+++ root/js/viewmodels/footer.js
@@ -5,12 +5,12 @@
 define(['ojs/ojcore', 'knockout'], function (oj, ko) {
     /**
      * The view model for the footer module.
-     * Used to display links on the bottom of the About Box dialog
+     * This module is used to display links on the bottom of the About Box dialog.
      */
     function FooterViewModel() {
         var self = this;
         self.footerLinks = ko.observableArray([
-            new footerLink('About Emxsys', 'aboutEmxsys', 'http://emxsys.com/about/index.html'),
+            new footerLink('About World Wind Explorer', 'aboutExplorer', 'http://emxsys.com/about/index.html'),
             new footerLink('Contact Us', 'contactUs', 'http://emxsys.com/contact/index.html'),
             new footerLink('Legal Notices', 'legalNotices', 'http://emxsys.com/policies/index.html'),
             new footerLink('Frequently Asked Questions', 'faqs', 'http://emxsys.com/faqs/index.html'),
